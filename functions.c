c831a81619b817bfd004edaf99395ce04b7a5f6a
Orio-Ola <samueloriola212@gmail.com>
2022-10-19 00:06:40 -0700

    Team project

--- functions.c
+++ functions.c
@@ -5,27 +5,20 @@
 /************************* PRINT UNSIGNED NUMBER *************************/
 
 /**
-
  * print_unsigned - Prints an unsigned number
-
  * @types: List a of arguments
-
  * @buffer: Buffer array to handle print
-
  * @flags:  Calculates active flags
-
  * @width: get width
-
  * @precision: Precision specification
-
  * @size: Size specifier
-
  * Return: Number of chars printed.
  */
 int print_char(va_list types, char buffer[],
 		int flags, int width, int precision, int size)
 {
 	char c = va_arg(types, int);
+
 	return (handle_write_char(c, buffer, flags, width, precision, size));
 }
 /************************* PRINT A STRING *************************/
